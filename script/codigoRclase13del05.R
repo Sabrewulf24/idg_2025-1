0310ac914c51d0e6892da0565064ed2a222ec12f
Sabrewulf24 <mathias.salas@hotmail.com>
2025-06-06 16:53:40 -0400

    clase06/06

--- script/codigoRclase13del05.R
+++ script/codigoRclase13del05.R
@@ -1,5 +1,11 @@
 install.packages("rakeR")
+install.packages("RPostgres")
+install.packages("ggplot2")
 library(rakeR)
+library(DBI)
+library(RPostgres)
+library(ggplot2)
+
 cons_censo_df <- readRDS("data/cons_censo_df.rds")
 casen_raw = readRDS("data/casen_rm.rds")
 View(cons_censo_df)
@@ -14,10 +20,7 @@ age_levels  <- grep("^edad", col_cons, value = TRUE)    # p.ej. "edad_menor_30",
 esc_levels  <- grep("^esco", col_cons, value = TRUE)    # p.ej. "esco_0","esco_1_8",…
 sexo_levels <- grep("^sexo_",col_cons, value = TRUE)    # p.ej. "sexo_f","sexo_m"
 
-age_levels = c(
-  "edad_menor_30",
-  setdiff(age_levels, "edad_menor_30")
-)
+
 # Sleccionar variables.. Se deben elimnar N.A
 
 vars_base = c("estrato", # para extraer ID de comuna
@@ -83,3 +86,69 @@ casen$sexo_cat <- factor(
          ifelse(casen$sexo == 1, sexo_levels[2], NA)), 
   levels = sexo_levels
 )
+
+
+
+## Microsimulación
+# crear la lista de constraints POR COMUNA
+cons_censo_comunas = split(cons_censo_df, cons_censo_df$COMUNA)
+
+# Lista de INDS 
+inds_list = split(casen, casen$Comuna)
+
+
+sim_list = lapply(names(cons_censo_comunas), function(zona) {
+  cons_i    = cons_censo_comunas[[zona]]
+  col_order = sort(setdiff(names(cons_i), c("COMUNA","GEOCODIGO")))
+  cons_i    = cons_i[, c("GEOCODIGO", col_order), drop = FALSE]
+  
+  tmp    = inds_list[[zona]]
+  inds_i = tmp[, c("ID","edad_cat","esc_cat","sexo_cat"), drop = FALSE]
+  names(inds_i) = c("ID","Edad","Escolaridad","Sexo")
+  
+  
+  
+  w_frac  = weight(cons = cons_i, inds = inds_i,
+                   vars = c("Edad","Escolaridad","Sexo"))
+  sim_i   = integerise(weights = w_frac, inds = inds_i, seed = 123)
+  merge(sim_i,
+        tmp[, c("ID","ypc")],
+        by = "ID", all.x = TRUE)
+})
+
+# Data Frame de toda la población
+sim_df = data.table::rbindlist(sim_list, idcol = "COMUNA")
+
+zonas_ypc = aggregate(
+  ypc ~ zone,
+  data = sim_df,
+  FUN  = function(x) median(x, na.rm = TRUE)
+)
+names(zonas_ypc) <- c("geocodigo", "mediana_ingreso")
+
+source("trabajos/t2_microsim/R/conexion_db.R")
+
+con = conectar_db("censo_rm_2017")
+
+dbWriteTable(
+  conn      = con,
+  name      = Id(schema = "dpa", table = "tmp_ingreso_rm"),
+  value     = zonas_ypc,
+  overwrite = TRUE,
+  row.names = FALSE
+)
+
+dbExecute(con, "CREATE INDEX ON dpa.tmp_ingreso_rm(geocodigo)")
+dbExecute(con, "ANALYZE dpa.tmp_ingreso_rm")
+
+# 1) Crea la nueva capa directamente con un SELECT … LEFT JOIN
+dbExecute(con, "
+  CREATE TABLE dpa.zonas_censales_gs_income AS
+  SELECT
+    z.*,
+    t.mediana_ingreso
+  FROM dpa.zonas_censales_rm AS z
+  LEFT JOIN dpa.tmp_ingreso_rm AS t
+    ON z.geocodigo::text = t.geocodigo
+WHERE urbano = 1 AND (nom_provin = 'SANTIAGO' OR nom_comuna = 'SAN BERNARDO' OR nom_comuna = 'PUENTE ALTO')
+")
